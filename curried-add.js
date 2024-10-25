2720cb301c6bf192365e7faa7193938895c7e9a2
Eric Moore <ericmoore0709@gmail.com>
2024-10-25 15:33:53 -0700

    curried-add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,12 @@
-function curriedAdd(total) {
+function curriedAdd(total = 0) {
+    if (arguments.length === 0) return total;
 
+    return function (num) {
+        if (num === undefined) {
+            return total;
+        }
+        return curriedAdd(total + num);
+    };
 }
 
 module.exports = { curriedAdd };
