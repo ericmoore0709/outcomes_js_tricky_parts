15fab7defe30699966080046ff3b46a7371ad874
Eric Moore <ericmoore0709@gmail.com>
2024-10-25 13:37:52 -0700

    guessing-game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,23 @@
 function guessingGame() {
-
+    const secret = Math.floor(Math.random() * 100);
+    let numGuesses = 0;
+    let won = false;
+    return function guess(guessNum) {
+        if (won) {
+            return "The game is over, you already won!";
+        }
+        numGuesses++;
+        if (guessNum === secret) {
+            won = true;
+            return `You win! You found ${secret} in ${numGuesses} guesses.`;
+        }
+        if (guessNum > secret) {
+            return `${guessNum} is too high!`;
+        }
+        else {
+            return `${guessNum} is too low!`;
+        }
+    }
 }
 
 module.exports = { guessingGame };
