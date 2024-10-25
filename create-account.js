c8dd3fe9f5ab58e96e3d0fed56530e84b8380edc
Eric Moore <ericmoore0709@gmail.com>
2024-10-25 13:58:47 -0700

    create-account

--- create-account.js
+++ create-account.js
@@ -1,4 +1,34 @@
-function createAccount(pin, amount) {
+function createAccount(startPin, initialAmount = 0) {
+
+    let currentPin = startPin;
+    let balance = initialAmount;
+
+    return {
+
+        checkBalance: function (pin) {
+            if (pin !== currentPin) return "Invalid PIN.";
+            return `$${balance}`;
+        },
+
+        deposit: function (pin, amount) {
+            if (pin !== currentPin) return "Invalid PIN.";
+            balance += amount;
+            return `Succesfully deposited $${amount}. Current balance: $${balance}.`;
+        },
+
+        withdraw: function (pin, amount) {
+            if (pin !== currentPin) return "Invalid PIN.";
+            if (amount > balance) return `Withdrawal amount exceeds account balance. Transaction cancelled.`;
+            balance -= amount;
+            return `Succesfully withdrew $${amount}. Current balance: $${balance}.`;
+        },
+
+        changePin: function (oldPin, newPin) {
+            if (oldPin !== currentPin) return "Invalid PIN.";
+            currentPin = newPin;
+            return `PIN successfully changed!`;
+        }
+    }
 
 }
 
