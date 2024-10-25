f337bee055c17450fcf63991aba5f30503bd2b17
Eric Moore <ericmoore0709@gmail.com>
2024-10-25 13:41:02 -0700

    guessing-game formatting cleanup

--- guessing-game.js
+++ guessing-game.js
@@ -3,20 +3,13 @@ function guessingGame() {
     let numGuesses = 0;
     let won = false;
     return function guess(guessNum) {
-        if (won) {
-            return "The game is over, you already won!";
-        }
+        if (won) return "The game is over, you already won!";
         numGuesses++;
         if (guessNum === secret) {
             won = true;
             return `You win! You found ${secret} in ${numGuesses} guesses.`;
         }
-        if (guessNum > secret) {
-            return `${guessNum} is too high!`;
-        }
-        else {
-            return `${guessNum} is too low!`;
-        }
+        return (guessNum > secret) ? `${guessNum} is too high!` : `${guessNum} is too low!`;
     }
 }
 
